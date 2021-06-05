18dec6869f36705728f1aa95a7e009a97a9a8eee
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-06-05 12:10:11 -0400

    Completed Lab 2 Unit testing

---
+++ IsItALeapYear/IsItALeapYear/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public Boolean IsItALeapYear(int year)
+        {
+            if (year % 400 == 0) return true;
+            if (year % 100 == 0) return false;
+            if (year % 4 == 0) return true;
+            return false;
+        }
+    }
+}
